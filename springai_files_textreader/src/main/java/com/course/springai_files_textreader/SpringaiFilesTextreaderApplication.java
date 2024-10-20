65535650eb3dec73109f71b9cae2f9787642a441
Praveen K Singh <c2praveen30jun@gmail.com>
2024-10-20 19:09:51 +0530

    Add files via upload

---
+++ springai_files_textreader/src/main/java/com/course/springai_files_textreader/SpringaiFilesTextreaderApplication.java
@@ -0,0 +1,13 @@
+package com.course.springai_files_textreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiFilesTextreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiFilesTextreaderApplication.class, args);
+	}
+
+}
